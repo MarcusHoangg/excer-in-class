133ac1eec64c2eaf0bebe8baf1c991669cba3059
MarcusHoangg <MINH.HOANG4@metropolia.fi>
2024-08-22 14:19:22 +0300

    exercise in the class

---
+++ Minhhoang3.py
@@ -0,0 +1,58 @@
+'''
+a = int(input("A is "))
+b = int(input("B is "))
+print(a)
+print(b)
+c = a
+a = b
+b=c
+print(a)
+print(b)
+'''
+import math
+
+#ex1
+'''
+name = input("Enter your name: ")
+print ("Nice to meet you ", name)
+'''
+#ex2
+'''
+rds = float(input("Enter your rds: "))
+area = math.pi*(rds)**2
+print("The area of your circle is ", area)
+'''
+#ex3
+'''
+length = float(input("Enter your length: "))
+width = float(input("Enter your width: "))
+perimeter = (length*width)*2
+print("Your perimeter is ", perimeter)
+'''
+#ex4
+'''
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+num3 = float(input("Enter your third number: "))
+sum=int(num1+num2+num3)
+average=int(sum/3)
+print("Your average is ", average)
+'''
+#ex5
+'''
+talent=float(input(f"Enter your talent: \n"))
+pounds=float(input(f"Enter your pounds: \n")) + (talent *20)
+lots= float(input(f"Enter yourlots: \n")) + (pounds *32)
+grams = lots*13.3
+kilograms= int((grams-(grams % 1000)) / 1000)
+grams_left = grams - (kilograms*1000)
+print("the weight in modern units:", grams_left)
+'''
+#ex6
+
+
+
+
+
+
+
